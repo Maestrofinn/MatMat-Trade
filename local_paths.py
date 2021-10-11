c464b84ec73dd3ed628db4858407e6e36052ad0c
Teixeira <teixeira@centre-cired.fr>
2021-10-11 18:34:22 +0200

    Set up main structure

---
+++ local_paths.py
@@ -0,0 +1,14 @@
+import pathlib
+import os
+import sys
+
+# settings
+dir_separator = os.sep
+current_pythonpath = sys.path
+sys.path.append(dir_separator.join(current_pythonpath[0].split(dir_separator)[:-1]))
+
+
+# repository
+base_dir = pathlib.Path(__file__).parents[0]
+data_dir = base_dir.joinpath('data')
+output_dir = base_dir.joinpath('outputs')
---
+++ main.py
@@ -0,0 +1,50 @@
+""" Python scripts in MatMat trade module
+
+    Notes
+    ------
+    Fill description
+
+    """
+
+# general
+import sys
+import os
+import copy
+
+# scientific
+import numpy as np
+import pandas as pd
+import pymrio
+
+# folder
+from local_paths import data_dir
+from local_paths import output_dir
+
+
+###########################
+# SETTINGS
+###########################
+base_year = 2015
+agg_name = {
+	'sector': 'reference',
+	'region': 'reference'
+}
+
+
+###########################
+# READ/ORGANIZE/CLEAN DATA
+###########################
+
+###########################
+# CALCULATIONS
+###########################
+
+###########################
+# FORMAT RESULTS
+###########################
+
+
+###########################
+# VISUALIZE
+###########################
+
